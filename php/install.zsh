7206b550f53ae74ba48289de6c04238b7acddb41
Paulo Rodrigues Pinto <regularjack@gmail.com>
2016-04-16 17:27:36 +0100

    Install php-xdebug and wp-cli

--- php/install.zsh
+++ php/install.zsh
@@ -5,12 +5,11 @@ PLATFORM=$(uname -s)
 if [[ "$PLATFORM" == "Darwin" ]]; then
   brew tap homebrew/php
   brew install php70 --with-postgresql --with-mysql
+  brew install php70-xdebug
 else
   echo 'PHP7 installation on Linux not implemented yet'
   return
 fi
-exit
-
 
 if [[ ! -a bin/composer ]]; then
   curl -sS https://getcomposer.org/installer | php -- --install-dir=bin --filename=composer
@@ -25,7 +24,7 @@ composer global update --prefer-dist
 
 # wp-cli fails to install with composer due to incompatible dependencies
 if [[ ! -a bin/wp ]]; then
-  curl -LsS https://symfony.com/installer -o bin/wp
+  curl -LsS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o bin/wp
   chmod +x bin/wp
 fi
 
