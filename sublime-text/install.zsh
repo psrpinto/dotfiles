212ae4a66371e11a9f7a1f330ec5fb3d5f5aae72
Paulo Rodrigues Pinto <regularjack@gmail.com>
2016-12-18 03:22:49 +0000

    [sublime] Move files to a User directory

--- sublime-text/install.zsh
+++ sublime-text/install.zsh
@@ -3,10 +3,10 @@
 PLATFORM=$(uname -s)
 
 if [[ "$PLATFORM" == "Linux" ]]; then
-  ln -sf ~/.dotfiles/sublime-text ~/.config/sublime-text-3/Packages/User
+  ln -sf ~/.dotfiles/sublime-text/User ~/.config/sublime-text-3/Packages/User
 fi
 
 if [[ "$PLATFORM" == "Darwin" ]]; then
   brew cask install sublime-text
-  ln -sfh ~/.dotfiles/sublime-text ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+  ln -sfh ~/.dotfiles/sublime-text/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 fi
