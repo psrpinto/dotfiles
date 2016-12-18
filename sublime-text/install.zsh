6f7cef632385b48c2aec4cad322025231ba8c6f6
Paulo Rodrigues Pinto <regularjack@gmail.com>
2016-12-18 03:37:10 +0000

    [sublime] Allow overriding Packages

--- sublime-text/install.zsh
+++ sublime-text/install.zsh
@@ -4,9 +4,16 @@ PLATFORM=$(uname -s)
 
 if [[ "$PLATFORM" == "Linux" ]]; then
   ln -sf ~/.dotfiles/sublime-text/User ~/.config/sublime-text-3/Packages/User
+
+  # TODO: Overrides
 fi
 
 if [[ "$PLATFORM" == "Darwin" ]]; then
   brew cask install sublime-text
   ln -sfh ~/.dotfiles/sublime-text/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+
+  for dir in ~/.dotfiles/sublime-text/Overrides/*;
+  do
+    ln -sfh $dir ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/$(basename $dir);
+  done
 fi
