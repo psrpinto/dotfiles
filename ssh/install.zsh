0d9973376c8cbc5ee8c2f8bae12d03734c8af52c
Paulo Rodrigues Pinto <regularjack@gmail.com>
2016-12-20 00:46:41 +0000

    [ssh] Remove host config from the repo

--- ssh/install.zsh
+++ ssh/install.zsh
@@ -1,4 +1,11 @@
 #!/usr/bin/env zsh
 
+PLATFORM=$(uname -s)
+
+if [[ "$PLATFORM" == "Darwin" ]]; then
+  brew install openssh
+fi
+
 mkdir -p ~/.ssh
 ln -sf ~/.dotfiles/ssh/sshconfig ~/.ssh/config
+ln -sfh ~/.dotfiles/ssh/config.d ~/.ssh/config.d
