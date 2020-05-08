aa7118d94df8740389a349cd7c1ce965b0bffc47
Paulo Rodrigues Pinto <regularjack@gmail.com>
2020-05-08 16:27:24 +0100

    Load API tokens

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,3 +4,11 @@ if [[ "$PLATFORM" == "Darwin" ]]; then
   LC_CTYPE=en_US.UTF-8
   LC_ALL=en_US.UTF-8
 fi
+
+if [[ -a ~/.github-packages-token ]]; then
+  export GITHUB_PACKAGES_TOKEN=$(cat ~/.github-packages-token)
+fi
+
+if [[ -a ~/.digitalocean-token ]]; then
+  export DIGITALOCEAN_TOKEN=$(cat ~/.digitalocean-token)
+fi
